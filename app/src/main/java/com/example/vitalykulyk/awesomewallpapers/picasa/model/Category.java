52d2ec7bd5779a8b4384315a90a789a41f0d6ede
Vitalii Kulyk <vitcool@ukr.net>
2016-03-13 22:42:47 +0200

    add classes (Part 1 finished)

---
+++ app/src/main/java/com/example/vitalykulyk/awesomewallpapers/picasa/model/Category.java
@@ -0,0 +1,32 @@
+package com.example.vitalykulyk.awesomewallpapers.picasa.model;
+
+/**
+ * Created by Vitaly Kulyk on 13.03.2016.
+ */
+public class Category {
+    public String id, title;
+
+    public Category() {
+    }
+
+    public Category(String id, String title) {
+        this.id = id;
+        this.title = title;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+}
